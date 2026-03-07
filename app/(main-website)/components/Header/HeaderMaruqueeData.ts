9634b4a260507031d44e137239d2845e5322da3d
= <=>
2026-03-07 16:59:04 +0530

    add marquee

---
+++ app/(main-website)/components/Header/HeaderMaruqueeData.ts
@@ -0,0 +1,10 @@
+import { HeroMarqueeSection } from "./MainHeaderMarquee";
+
+export const heroMarqueeData: HeroMarqueeSection = {
+  messages: [
+         "Registration Open for 7th Convocation 2026 on 14th March 2026 (Saturday) at K.R Mangalam University. Registration will close by 12th March 2026.",
+         "Registration Open for 7th Convocation 2026 on 14th March 2026 (Saturday) at K.R Mangalam University. Registration will close by 12th March 2026.",
+  ],
+  bgColor: "#e31e24",
+  speedClass: "mobanimate-marquee",
+};
