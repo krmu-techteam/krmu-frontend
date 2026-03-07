7ac5b414801cf1b8813c5a91266be586004437cd
krmu-techteam <tech@krmangalam.com>
2026-03-07 15:20:19 +0530

    strapi and krmuold api hardcoded ur and landing pahge jmc change

--- app/(landing-page)/admission/journalism-and-mass-communication-2026/content.ts
+++ app/(landing-page)/admission/journalism-and-mass-communication-2026/content.ts
@@ -32,7 +32,7 @@ export const Hero: HeroContent = {
       PROGRAMMES <br /> 2026-27
       </span>
     `,
-  desc: `The Emerging Media & Creator Economy courses <br />focuses on developing critical <br /> thinking, communication skills, creativity, <br /> and social awareness, preparing students <br /> for the fast-changing world of media, <br /> communication and storytelling`,
+  desc: `The Emerging Media & Creator Economy courses <br />focus on developing critical <br /> thinking, communication skills, creativity, <br /> and social awareness, preparing students <br /> for the fast-changing world of media, <br /> communication and storytelling`,
   heroBg: "/wp-content/landingpage/sjmc-2026/sjmc.webp",
   // middleImg: "/landingpage/law-2026/law.webp",
   middleImg: "",
@@ -250,7 +250,7 @@ export const opportunityData: OpportunitySection = {
   heading: "Learn From Industry Experts",
   highlight: "Opportunity That Transforms",
   cta: {
-    label: "Apply for JMC Programme 2026-2027",
+    label: "Apply for EMCE Programme 2026-2027",
     href: "#",
   },
 };
@@ -476,7 +476,7 @@ export const applySectionData: ApplySectionData = {
 
 export const programmeContent = {
   badge: "3 Specialisations",
-  title: "KRMU’s Emerging Media & Creator Economy",
+  title: "KRMU’s Emerging Media & Creator Economy Programmes",
   subtitle: "All Our Programmes Comprise of Industry-relevant Curriculum",
 
   programmes: [
