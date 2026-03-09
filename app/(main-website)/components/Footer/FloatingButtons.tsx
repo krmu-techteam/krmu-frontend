f09ed5a41be82f82f31f1836c0986ff781d2983d
krmu-techteam <tech@krmangalam.com>
2026-03-09 11:58:08 +0530

    new floating buttons componnent and logic creation for4 univiser

---
+++ app/(main-website)/components/Footer/FloatingButtons.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function FloatingButtons() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {pathname !== "/univiser" && (
+        <Link
+          href="/univiser"
+          className="bg-[#f00] text-white py-[6px] px-[15px] fixed top-[40%] right-[-120px] rotate-90 rounded-sm h-[27px] z-10"
+        >
+          Connect With Campus Pioneer
+        </Link>
+      )}
+
+      <Link
+        href="/campus-life/virtual-tour"
+        className="bg-[#f00] text-white py-[6px] px-[15px] fixed top-[70%] right-[-57px] rotate-90 rounded-sm h-[27px] z-10"
+      >
+        360° Virtual Tour
+      </Link>
+    </>
+  );
+}
