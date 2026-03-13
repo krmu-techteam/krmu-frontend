79ea9a48ddb263e4f8ca313068efa58a3632962a
krmu-techteam <tech@krmangalam.com>
2026-03-13 11:17:49 +0530

    fcl up

--- lib/api/school-faculties/smas.ts
+++ lib/api/school-faculties/smas.ts
@@ -293,12 +293,12 @@ export const smasFaculties: SchoolFacultiesType[] = [
     id: 8942,
     slug: "mr-mohit-agarwal",
     title: {
-      rendered: "Mr. Mohit Agarwal",
+      rendered: "Dr. Mohit Agarwal",
     },
     featured_media: 8943,
     acf: {
-      "staff-qualification": "Ph.D. in Pharmaceutical Science (Pursuing) ",
-      staff_designation: " Assistant Professor",
+      "staff-qualification": "M.Pharm, Ph.D. (Pharmaceutical sciences)",
+      staff_designation: " Assistant Professor (Senior Scale)",
       schools: [1098],
     },
     featured_media_url:
