788e3e744bbbffc626d219d9280d96593ff10320
krmu-techteam <tech@krmangalam.com>
2026-03-14 16:27:57 +0530

    fee string type error solvedx

---
+++ app/(landing-page)/admission/btech-2026-demo/seo.ts
@@ -0,0 +1,6 @@
+export const SeoData = {
+  title: "B.Tech. KRMU Admissions Open 2025-26",
+  description:
+    "Apply for B.Tech. Admissions 2026",
+  shareImage: "https://www.krmangalam.edu.in/wp-content/landingpage/common/krmlogo.webp",
+};
