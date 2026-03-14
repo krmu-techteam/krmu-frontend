8ca75c2361d01f5071b7420b8e16152e32404f81
krmu-techteam <tech@krmangalam.com>
2026-03-14 16:17:55 +0530

    deleted btech demo page landing page

--- app/(landing-page)/admission/btech-2026-demo/seo.ts
+++
@@ -1,6 +0,0 @@
-export const SeoData = {
-  title: "B.Tech. KRMU Admissions Open 2025-26",
-  description:
-    "Apply for B.Tech. Admissions 2026",
-  shareImage: "https://www.krmangalam.edu.in/wp-content/landingpage/common/krmlogo.webp",
-};
