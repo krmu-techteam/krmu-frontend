5523ebe61e56703d0b9e56424af75fe2ff68cff8
krmu-techteam <tech@krmangalam.com>
2026-03-16 11:55:08 +0530

    seo tile and desxc updated shared by bijender for some pages

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,6 @@
 export const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL || "";
 export const FETCH_STRAPI_URL = process.env.NEXT_PUBLIC_FETCH_STRAPI_URL || "";
-// export const STRAPI_URL = "http://localhost:1337";
+// expo rt const STRAPI_URL = "http://localhost:1337";
 // export const FETCH_STRAPI_URL = "http://localhost:1337";
 // export const FETCH_STRAPI_URL = "http://techapi.krmangalam.edu.in";
 
