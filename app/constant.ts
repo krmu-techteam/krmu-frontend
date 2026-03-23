ad0e80872402ee935203597a52c15b9336ae1f07
krmu-techteam <tech@krmangalam.com>
2026-03-23 13:21:04 +0530

    headre scroll reove

--- app/constant.ts
+++ app/constant.ts
@@ -2,7 +2,7 @@ export const STRAPI_URL = "";
 export const FETCH_STRAPI_URL = process.env.NEXT_PUBLIC_FETCH_STRAPI_URL || "";
 // expo rt const STRAPI_URL = "http://localhost:1337";
 // export const FETCH_STRAPI_URL = "http://localhost:1337";
-// export const F ETCH_STRAPI_URL = "http://techapi.krmangalam.edu.in";
+// export const F  ETCH_STRAPI_URL = "http://techapi.krmangalam.edu.in";
 
 export const KRMUWordUrl = process.env.NEXT_PUBLIC_KRMU_WORD_URL || "";
 export const origUrl = process.env.NEXT_PUBLIC_MAIN_URL || "";
