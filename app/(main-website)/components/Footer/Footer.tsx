7071326bed48f8e68a902add8769ba58a6ce2413
= <=>
2026-03-24 12:51:03 +0530

    try to fix issue

--- app/(main-website)/components/Footer/Footer.tsx
+++ app/(main-website)/components/Footer/Footer.tsx
@@ -196,6 +196,21 @@ const Footer = async () => {
         />
       </div>
       <NpfChatbot />
+        <div className="fixed bottom-0 left-0 w-full text-center z-50 flex sm:hidden items-center justify-center ">
+        <Link
+          href="https://admissions.krmangalam.edu.in/"
+          target="_blank"
+          className="bg-[#0a41a1] w-1/2 rounded-xl border border-white  text-white py-2.5 inline-block cursor-pointer"
+        >
+          Apply Now
+        </Link>
+        <NpfPopup
+          formId="d63cf9c4d3104c39f3ac28164701a69c"
+          btnClass={`bg-[#0a41a1] w-1/2 rounded-xl border border-white  text-white py-2.5 inline-block cursor-pointer npfWidget-d63cf9c4d3104c39f3ac28164701a69c `}
+          btnText="Enquire Now"
+          showIcon={false}
+        />
+      </div>
     </>
   );
 };
