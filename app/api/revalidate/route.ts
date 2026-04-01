2a130d3dad235810d5152945dd3ddc17883365c8
princecodecfart <princesharmabbu0@gmail.com>
2026-04-01 11:42:00 +0530

    fixed bug

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: NextRequest) {
 
   try {
     // Revalidate all fetches tagged with "blogs"
-    revalidateTag("blogs");
+    revalidateTag("blogs", "page");
     // Also revalidate the blog pages themselves
     revalidatePath("/blog", "layout");
 
