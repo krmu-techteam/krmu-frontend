d75d96360bb1cbcfbbc896e8047c3ac3bf35ed1c
princecodecfart <princesharmabbu0@gmail.com>
2026-04-09 15:43:10 +0530

    feat: add PHDAdmission component to render program details via Strapi blocks

--- app/(main-website)/(school-programmes)/programs/phd/comp/PHDAdmission.tsx
+++ app/(main-website)/(school-programmes)/programs/phd/comp/PHDAdmission.tsx
@@ -19,14 +19,14 @@ const PHDAdmission = ({ title1, title2, desc1, desc2 }: Props) => {
     >
       <div className="max-w-[1664px] mx-auto w-full flex flex-col lg:flex-row items-center">
         <div className="lg:w-1/2 md:px-[100px] text-white">
-          <div className="text-5xl text-white font-semibold mb-10">
+          <div className="text-3xl md:text-5xl text-white font-semibold mb-10">
             <BlocksRenderer content={title1} />
           </div>
           <div className="admission-process-section relative p-5">
             <BlocksRenderer content={desc1} />
           </div>
         </div>
-        <div className="lg:w-1/2 md:px-[100px] text-white">
+        <div className="lg:w-1/2 md:px-[100px] text-white mt-5">
           <div className="text-3xl md:text-5xl text-white font-semibold mb-5 md:mb-10">
             <BlocksRenderer content={title2} />
           </div>
