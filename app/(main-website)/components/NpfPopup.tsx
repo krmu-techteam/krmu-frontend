66dff8ee4f546b480ad1e25991c6c70e5b943c49
= <=>
2026-04-14 15:08:55 +0530

    change npf popup code as before because form is not working on website

--- app/(main-website)/components/NpfPopup.tsx
+++ app/(main-website)/components/NpfPopup.tsx
@@ -24,18 +24,17 @@ const NpfPopup = ({
 
     loadNpfScript()
       .then(() => {
-        if (typeof window !== "undefined" && (window as any).NpfWidgetsInit) {
-          new (window as any).NpfWidgetsInit({
-            widgetId: formId,
-            baseurl: "widgets.nopaperforms.com",
-            formTitle: " ",
-            titleColor: "#ffffff",
-            backgroundColor: "#ffffff",
-            iframeHeight: "520px",
-            buttonTextColor: "#FFF",
-            target: btnRef.current,
-          });
-        }
+        // @ts-expect-error - test
+        new NpfWidgetsInit({
+          widgetId: formId,
+          baseurl: "widgets.nopaperforms.com",
+          formTitle:  "Apply Now",
+          titleColor: "#FF0033",
+          backgroundColor: "#ddd",
+          iframeHeight: "500px",
+          buttonTextColor: "#FFF",
+          target: btnRef.current,
+        });
       })
       .catch((err) => {
         console.error("NPF init failed:", err);
@@ -50,3 +49,58 @@ const NpfPopup = ({
 };
 
 export default NpfPopup;
+
+
+// commented by rohit because popup form is not working on complete website - 15:06 - 14-04-2026
+// "use client";
+
+// import { loadNpfScript } from "@/lib/constants/loadNpfScript";
+// import { MoveRight } from "lucide-react";
+// import { useEffect, useRef } from "react";
+
+// type Props = {
+//   formId?: string;
+//   btnClass?: string;
+//   btnText?: string;
+//   showIcon?: boolean;
+// };
+
+// const NpfPopup = ({
+//   formId,
+//   btnClass = "",
+//   btnText = "Apply Now",
+//   showIcon = false,
+// }: Props) => {
+//   const btnRef = useRef<HTMLButtonElement>(null);
+
+//   useEffect(() => {
+//     if (!formId || !btnRef.current) return;
+
+//     loadNpfScript()
+//       .then(() => {
+//         if (typeof window !== "undefined" && (window as any).NpfWidgetsInit) {
+//           new (window as any).NpfWidgetsInit({
+//             widgetId: formId,
+//             baseurl: "widgets.nopaperforms.com",
+//             formTitle: " ",
+//             titleColor: "#ffffff",
+//             backgroundColor: "#ffffff",
+//             iframeHeight: "520px",
+//             buttonTextColor: "#FFF",
+//             target: btnRef.current,
+//           });
+//         }
+//       })
+//       .catch((err) => {
+//         console.error("NPF init failed:", err);
+//       });
+//   }, [formId, btnText]);
+
+//   return (
+//     <button ref={btnRef} className={`${btnClass} cursor-pointer`}>
+//       {btnText} {showIcon && <MoveRight size={16} />}
+//     </button>
+//   );
+// };
+
+// export default NpfPopup;
