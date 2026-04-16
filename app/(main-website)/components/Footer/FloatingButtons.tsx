c7d20c18a1f370d57b0e9151503521fd82d50f0c
= <=>
2026-04-16 16:26:24 +0530

    fix button issue

--- app/(main-website)/components/Footer/FloatingButtons.tsx
+++ app/(main-website)/components/Footer/FloatingButtons.tsx
@@ -26,7 +26,7 @@ export default function FloatingButtons() {
 
       <Link
   href="/campus-life/virtual-tour"
-  className="bg-[#f00] text-white py-[2px] px-[15px] fixed top-[70%] right-[-62px] rotate-90 rounded-sm h-[27px] z-10"
+  className="bg-[#f00] text-white py-[2px] px-[15px] fixed top-[60%] right-[-62px] rotate-90 rounded-sm h-[27px] z-10"
 >
   360° Virtual Tour
 </Link>
