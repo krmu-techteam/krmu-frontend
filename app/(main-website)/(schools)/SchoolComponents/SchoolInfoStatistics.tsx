d75ea390ca5ff451f85775a5e4cdd7d0713668dd
princecodecraft <princesharmabbu0@gmail.com>
2026-05-06 23:40:34 +0530

    feat: add SchoolInfoStatistics component and integrate it into the dynamic school detail page

--- app/(main-website)/(schools)/SchoolComponents/SchoolInfoStatistics.tsx
+++ app/(main-website)/(schools)/SchoolComponents/SchoolInfoStatistics.tsx
@@ -15,18 +15,18 @@ const infoStaticImages: Record<string, string> = {
   "school-of-engineering-and-technology":
     "https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/soet_info_d088e58b5e.webp",
   "school-of-management-and-commerce":
-    "https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/somc_info_9a4d158e6c.webp",
+    "https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/school_of_management_and_commerce_c064a2d99d.png",
   "school-of-legal-studies":
     "https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/sols_info_356ffbbb2f.webp",
-    'school-of-medical-and-allied-sciences' : 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/smas_info_a437a7a78f.webp',
-    'school-of-physiotherapy-and-rehabilitation-sciences': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/sprs_info_ab151f554d.webp',
-    'school-of-liberal-arts': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/sola_info_0bea1c1947.webp',
-    'school-of-architecture-design': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/soad_info_fb5a40c9cc.png',
-    'school-of-basic-and-applied-sciences': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/sbas_info_a4df052f90.webp',
-    'school-of-journalism-and-mass-communication': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/sjmc_info_4152524039.webp',
-    'school-of-hotel-management-and-catering-technology' : 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/shmct_info_902ba1a177.webp',
-    'school-of-agriculutural-sciences': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/soas_info_fc180558e4.webp',
-    'school-of-education' : 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/soed_info_bc9daec6fd.webp'
+  'school-of-medical-and-allied-sciences': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/smas_info_a437a7a78f.webp',
+  'school-of-physiotherapy-and-rehabilitation-sciences': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/sprs_info_ab151f554d.webp',
+  'school-of-liberal-arts': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/sola_info_0bea1c1947.webp',
+  'school-of-architecture-design': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/soad_info_fb5a40c9cc.png',
+  'school-of-basic-and-applied-sciences': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/sbas_info_a4df052f90.webp',
+  'school-of-journalism-and-mass-communication': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/sjmc_info_4152524039.webp',
+  'school-of-hotel-management-and-catering-technology': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/shmct_info_902ba1a177.webp',
+  'school-of-agriculutural-sciences': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/soas_info_fc180558e4.webp',
+  'school-of-education': 'https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/soed_info_bc9daec6fd.webp'
 
 };
 
