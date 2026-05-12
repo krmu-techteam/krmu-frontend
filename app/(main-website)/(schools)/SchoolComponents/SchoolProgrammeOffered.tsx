4041d73048f2204e2d7dc1d909ab56b0c20c5ac1
= <=>
2026-05-12 14:51:21 +0530

    school programme offered gradient section

--- app/(main-website)/(schools)/SchoolComponents/SchoolProgrammeOffered.tsx
+++ app/(main-website)/(schools)/SchoolComponents/SchoolProgrammeOffered.tsx
@@ -15,10 +15,10 @@ const SchoolProgrammeOffered = ({
 }: Props) => {
   return (
     <>
-      <section id="Explore-Courses">
+      <section id="Explore-Courses" className="pt-10 md:pt-20">
         <div className="max-w-[1664px] mx-auto w-full">
           <div className="text-center max-w-5xl mx-auto w-full px-4">
-            <h3 className="text-3xl mb-2 md:text-4xl lg:text-5xl font-semibold">
+            <h3 className="text-3xl mb-5 md:text-4xl lg:text-5xl font-semibold">
               {title}
             </h3>
             <p>{content}</p>
