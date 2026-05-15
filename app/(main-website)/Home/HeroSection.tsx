aede02f0ddd3f556d13fe37dea3a5b5e02d149ec
vivek verma <vivek459verma@gmail.com>
2026-05-15 17:47:37 +0530

    fix: improve styling and formatting in HeroSection component

--- app/(main-website)/Home/HeroSection.tsx
+++ app/(main-website)/Home/HeroSection.tsx
@@ -1,22 +1,21 @@
-
 import { StrapiMedia } from "@/lib/types/common";
 import Video from "../components/Videos/Video";
 
 interface HeroSectionProps {
   title: string;
   subtitle: string;
-  video: StrapiMedia | null 
-} 
-const HeroSection = ({title, subtitle, video}: HeroSectionProps) => {
+  video: StrapiMedia | null;
+}
+const HeroSection = ({ title, subtitle, video }: HeroSectionProps) => {
   return (
     <>
       <section className="relative overflow-hidden testing">
         <Video video={video} />
         <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center flex-col text-center text-white px-4 md:px-0">
-          <p className="text-xl md:text-2xl font-light">
+          <p className="text-xl md:text-3xl font-medium text-shadow-lg">
             {subtitle}
           </p>
-          <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold mt-4">
+          <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold mt-4 text-shadow-lg">
             {title}
           </h1>
         </div>
