f599d2de967c739c06c3bdd02f454add38dffc6b
= <=>
2026-05-16 12:34:10 +0530

    try to add npf chatbot code fix

--- app/NpfAgent.tsx
+++ app/NpfAgent.tsx
@@ -6,8 +6,17 @@ export default function NpfAgent() {
   return (
     <>
       <Script
-        src="https://z62gj5o2lx.in1.agent.nopaperforms.com/en-gb/backend/agents/aiagentscpt.js/6416018f1c55272a/cc38ad8a3a50478e8cd17f8ff4cafc87"
+        id="npf-agent-script"
         strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var s = document.createElement("script");
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://z62gj5o2lx.in1.agent.nopaperforms.com/en-gb/backend/agents/aiagentscpt.js/6416018f1c55272a/cc38ad8a3a50478e8cd17f8ff4cafc87";
+            document.body.appendChild(s);
+          `,
+        }}
       />
 
       <div
