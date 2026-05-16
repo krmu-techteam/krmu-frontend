7c42537ff0e346cb48ccf8ec0e492e767124a5f9
= <=>
2026-05-16 12:23:22 +0530

    update npf agen

--- app/NpfAgent.tsx
+++ app/NpfAgent.tsx
@@ -6,17 +6,8 @@ export default function NpfAgent() {
   return (
     <>
       <Script
-        id="npf-agent-script"
+        src="https://z62gj5o2lx.in1.agent.nopaperforms.com/en-gb/backend/agents/aiagentscpt.js/6416018f1c55272a/cc38ad8a3a50478e8cd17f8ff4cafc87"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var s = document.createElement("script");
-            s.type = "text/javascript";
-            s.async = true;
-            s.src = "https://z62gj5o2lx.in1.agent.nopaperforms.com/en-gb/backend/agents/aiagentscpt.js/6416018f1c55272a/cc38ad8a3a50478e8cd17f8ff4cafc87";
-            document.body.appendChild(s);
-          `,
-        }}
       />
 
       <div
