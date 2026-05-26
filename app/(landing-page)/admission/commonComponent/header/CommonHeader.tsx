9f1bc22be73e36d7611fc5ad5ac93d772540db7e
princecodecfart <princesharmabbu0@gmail.com>
2026-05-26 11:15:38 +0530

    feat: add CommonHeader component and KRMUTimer countdown for landing page admission section

--- app/(landing-page)/admission/commonComponent/header/CommonHeader.tsx
+++ app/(landing-page)/admission/commonComponent/header/CommonHeader.tsx
@@ -31,7 +31,7 @@ export const CommonHeader = () => {
             Apply Now
           </button> */}
           {/* <AnimatedCounter targetDate="February 28, 2026 6:30 PM" /> */}
-          <KRMUTimer targetDate="" />
+          <KRMUTimer targetDate="2026-05-31T23:59:59" />
 
           <ScrollButton
             targetId="apply-section"
