8ef51f9331da8fde1bcaa865e6d1fb5992dfe175
vivek verma <vivek459verma@gmail.com>
2026-05-27 16:19:36 +0530

    feat: update styles and layout in various components including PHDAdmissionAccordion, Hero, OverviewHearit, OverviewTestimonial, PHDHighlight, PHDProgrammeHighlight, and SchoolKnowledgeLogos

--- app/(main-website)/(life-at-krmu)/overview/comp/OverviewTestimonial.tsx
+++ app/(main-website)/(life-at-krmu)/overview/comp/OverviewTestimonial.tsx
@@ -32,7 +32,7 @@ const OverviewTestimonial = ({ testiData }: TestimonialProp) => {
           {testiData &&
             testiData.map((item) => {
               return (
-                <CarouselItem key={item?.id} className="md:basis-1/2 pt-4 flex">
+                <CarouselItem key={item?.id} className="md:basis-1/2 flex">
                   <OverviewTestiCard
                     profilename={item?.name}
                     info={item?.info}
@@ -43,12 +43,8 @@ const OverviewTestimonial = ({ testiData }: TestimonialProp) => {
               );
             })}
         </CarouselContent>
-        <CarouselPrevious
-          className="bg-[#051630] text-white hover:bg-[#0060aa] border-none w-[50px] h-[50px] md:w-[60px] md:h-[60px] top-[110%] left-[20%] sm:left-[30%] xl:top-[85%] xl:left-[-66.5%]"
-        />
-        <CarouselNext
-          className="bg-[#051630] text-white hover:bg-[#0060aa] border-none w-[50px] h-[50px] md:w-[60px] md:h-[60px] top-[110%] right-[20%] sm:right-[30%] xl:top-[85%] xl:left-[-56.5%]"
-        />
+        <CarouselPrevious className="bg-[#051630] text-white hover:bg-[#0060aa] border-none w-[50px] h-[50px] md:w-[60px] md:h-[60px] top-[107%] left-[20%] sm:left-[30%] xl:top-[85%] xl:left-[-66.5%]" />
+        <CarouselNext className="bg-[#051630] text-white hover:bg-[#0060aa] border-none w-[50px] h-[50px] md:w-[60px] md:h-[60px] top-[107%] right-[20%] sm:right-[30%] xl:top-[85%] xl:left-[-56.5%]" />
       </Carousel>
     </>
   );
