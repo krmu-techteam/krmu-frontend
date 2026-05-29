db9ec3f036e6a6eb3ced4876a613b9a6705630e2
= <=>
2026-05-29 15:21:52 +0530

    update images

--- app/(main-website)/test/page.tsx
+++ app/(main-website)/test/page.tsx
@@ -3,49 +3,137 @@ import Image from "next/image";
 const page = () => {
   return (
     <div className="mt-20 py-20 grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-6 gap-5 max-w-[1440px] mx-auto px-5">
-      <div className="border border-gray-200">
+      <div className="border border-gray-200 flex items-center justify-center">
         <Image
-          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Adtiya_Birla_Group_Logo_f87b5ee50b.png"
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Asianpaint_2_8ba83d6d0f.png"
           width={250}
           height={80}
           alt=""
         />
       </div>
-      <div className="border border-gray-200">
+      <div className="border border-gray-200 flex items-center justify-center">
         <Image
-          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/AIIMS_logo_189ff03744.png"
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/AIIMS_logo_751e0bf512.png"
           width={250}
           height={80}
           alt=""
         />
       </div>
-      <div className="border border-gray-200">
+      <div className="border border-gray-200 flex items-center justify-center">
         <Image
-          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Adidas_logo_685a00ae39.png"
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Adidas_logo_5bf1c6c534.png"
           width={250}
           height={80}
           alt=""
         />
       </div>
-      <div className="border border-gray-200">
+      <div className="border border-gray-200 flex items-center justify-center">
         <Image
-          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Aimil_Logo_2f51d3c240.png"
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Asianpaint_5bb43fb5cd.png"
           width={250}
           height={80}
           alt=""
         />
       </div>
-      <div className="border border-gray-200">
+      <div className="border border-gray-200 flex items-center justify-center">
         <Image
-          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Airtel_Logo_471cb5be51.png"
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Autodesk_9669e489cb.png"
           width={250}
           height={80}
           alt=""
         />
       </div>
-      <div className="border border-gray-200">
+      <div className="border border-gray-200 flex items-center justify-center">
         <Image
-          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Adobe_Logo_4ef3d7dc42.png"
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Atlassain_028defc539.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Amazon_e967593533.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Ashai_India_Glass_85cb910dce.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Airtel_Logo_343aa9007a.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Airtel_Logo_2_43d9e1ba01.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Akal_41b419e167.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Alkem_518800a0f8.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Aimil_Logo_e6d6b258bc.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Asain_city_hospital_7f8ce073cb.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Adtiya_Birla_Group_Logo_4daeae5216.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/apollo_ebf83a9200.png"
+          width={250}
+          height={80}
+          alt=""
+        />
+      </div>
+      <div className="border border-gray-200 flex items-center justify-center">
+        <Image
+          src="https://truthful-cabbage-82fd27e8f6.media.strapiapp.com/Adobe_Logo_6ec1e46e91.png"
           width={250}
           height={80}
           alt=""
