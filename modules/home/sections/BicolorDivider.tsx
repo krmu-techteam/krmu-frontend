37134539c3b03fb3cb259a4782548102081b80da
princecodecfart <princesharmabbu0@gmail.com>
2026-05-30 10:44:38 +0530

    feat: add BicolorDivider component and export it through home module barrel files

---
+++ modules/home/sections/BicolorDivider.tsx
@@ -0,0 +1,10 @@
+const BicolorDivider = () => {
+  return (
+    <div className="h-2 w-full flex">
+      <div className="w-1/2 bg-[#0055a4]"></div>
+      <div className="w-1/2 bg-[#ed1c24]"></div>
+    </div>
+  );
+};
+
+export default BicolorDivider;
