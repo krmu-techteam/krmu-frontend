7429bfa8b9bca54275cfc6dc4022c776b1bf39eb
= <=>
2026-06-04 10:11:48 +0530

    highlight topbar menu

--- app/(main-website)/components/Navbar/TopbarMenu.tsx
+++ app/(main-website)/components/Navbar/TopbarMenu.tsx
@@ -32,7 +32,7 @@ const TopbarMenu = ({ topbarmenu }: TopbarProps) => {
 
         if (item.__component === "menu.menu-links") {
           return (
-            <li key={item.id}>
+            <li key={item.id} className="first:bg-[#001732] first:px-2 first:py-[2px]">
               {(item.url) ? (
                 <Link
                   href={item.url}
