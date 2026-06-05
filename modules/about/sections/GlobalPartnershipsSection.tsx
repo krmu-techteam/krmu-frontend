1c29eff41e9c7f5d4079e51403597228e8732c36
princecodecfart <princesharmabbu0@gmail.com>
2026-06-05 10:49:23 +0530

    feat: add GlobalPartnershipsSection component to about module

--- modules/about/sections/GlobalPartnershipsSection.tsx
+++ modules/about/sections/GlobalPartnershipsSection.tsx
@@ -22,10 +22,10 @@ interface GlobalPartnershipsSectionProps {
 }
 const GlobalPartnershipsSection = ({ aboutData }: GlobalPartnershipsSectionProps) => {
     return (
-        <section className="py-8 sm:py-10 lg:py-12 xl:py-16 px-6 sm:px-10 lg:px-12 xl:px-16 bg-transparent relative">
-        <div className="max-w-[1530px] mx-auto w-full bg-gradient-to-r from-[#061623] via-[#061623] to-[#061623]/20 rounded-[4px] flex flex-col lg:flex-row items-stretch overflow-hidden">
-          <div className="w-full lg:w-[60%] text-white p-10 xl:p-14 flex flex-col justify-center">
-            <h3 className="text-3xl md:text-[42px] font-serif font-semibold w-full leading-[1.2] mb-6">
+        <section className="py-8 sm:py-10 lg:py-12 xl:py-16 px-0 sm:px-10 lg:px-12 xl:px-16 bg-transparent relative">
+        <div className="max-w-[1530px] mx-auto w-full bg-gradient-to-r from-[#061623] via-[#061623] to-[#061623]/20 rounded-[4px] flex flex-col-reverse lg:flex-row items-stretch overflow-hidden">
+          <div className="w-full lg:w-[60%] text-white p-6 lg:p-10 xl:p-14 flex flex-col justify-center">
+            <h3 className="text-3xl text-center lg:text-left md:text-[42px] font-serif font-semibold w-full leading-[1.2] mb-6">
               {aboutData?.internationcollaboration?.title}
             </h3>
             <div className="text-[14px] text-justify sm:text-[15px] leading-relaxed text-white/90 mb-0 font-poppins pr-0 lg:pr-10">
@@ -36,7 +36,7 @@ const GlobalPartnershipsSection = ({ aboutData }: GlobalPartnershipsSectionProps
             {(aboutData?.internationcollaboration?.intcollabbtn?.buttonlink ||
               aboutData?.internationcollaboration?.intcollabbtn
                 ?.buttonclass) && (
-              <div className="mb-10">
+              <div className="mb-10 flex justify-center lg:justify-start">
                 <Link
                   href={
                     aboutData?.internationcollaboration?.intcollabbtn
@@ -62,7 +62,7 @@ const GlobalPartnershipsSection = ({ aboutData }: GlobalPartnershipsSectionProps
               />
             </div>
           </div>
-          <div className="w-full lg:w-[40%] relative flex items-center lg:items-end justify-center lg:justify-end mt-0 lg:mt-0 pt-0 lg:pt-0">
+          <div className="w-full lg:w-[40%] relative flex items-center lg:items-end justify-center lg:justify-end mt-0 lg:mt-0 pt-0 lg:pt-0 [mask-image:linear-gradient(to_bottom,black_80%,transparent_100%)] lg:[mask-image:none] px-2">
             <Image
               src="/modules/about/international-collaborations/international-collaborations.png"
               width={600}
