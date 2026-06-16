84561e56f96e5923eb9d59559f0585266e6b23e7
= <=>
2026-06-16 15:53:05 +0530

    enable author name and profile page

--- app/(main-website)/(blogs)/blog/(listings)/single-blog-comp/SingleBlogHeroAuthor.tsx
+++ app/(main-website)/(blogs)/blog/(listings)/single-blog-comp/SingleBlogHeroAuthor.tsx
@@ -15,59 +15,54 @@ const SingleBlogHeroAuthor = ({
   imgId,
   authorSlug,
 }: Props) => {
+  return (
+    <div className="single_blog_author_container">
+      <div className="single_blog_author_image">
+        <Image src="/blogs/blog-author.webp" width={50} height={50} alt="" />
+
+        {/* {imgId && <SingleBlogAuthorImage imgId={imgId} />} */}
+      </div>
+      <div className="single_blog_author_info">
+        <span className="author_name">
+          {authorName === "KRMU Team" ? (
+            <span className="text-white">{authorName}</span>
+          ) : (
+            <Link
+              href={`/blog/author/${authorSlug}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              {authorName}
+            </Link>
+          )}
+        </span>
+        <span className="author_designation">{desg}</span>
+      </div>
+    </div>
+  );
   // return (
-  //   <div className="single_blog_author_container">
+  //   <div className="single_blog_author_container text-center">
+
   //     <div className="single_blog_author_image">
   //       <Image
   //         src="/blogs/blog-author.webp"
   //         width={50}
   //         height={50}
-  //         alt=""
+  //         alt="Author"
+  //         className="author_img"
   //       />
-
-  //       {/* {imgId && <SingleBlogAuthorImage imgId={imgId} />} */}
   //     </div>
+
   //     <div className="single_blog_author_info">
-  //       <span className="author_name">
-  //         <span>
-  //           KRMU Team
-  //         </span>
-  //         {/* <Link href={`/author/${authorSlug}`} target="_blank" rel="noopener noreferrer">
-  //           {authorName}
-  //         </Link> */}
+  //       <span className="author_name text-black lg:text-white">
+  //         KRMU Team
   //       </span>
-  //       {/* <span className="author_designation">{desg}</span> */}
+
   //     </div>
+
   //   </div>
-
   // );
-  return (
-    <div className="single_blog_author_container text-center">
-
-      <div className="single_blog_author_image">
-        <Image
-          src="/blogs/blog-author.webp"
-          width={50}
-          height={50}
-          alt="Author"
-          className="author_img"
-        />
-      </div>
-
-      <div className="single_blog_author_info">
-        <span className="author_name text-black lg:text-white">
-          KRMU Team
-        </span>
-
-        {/* Optional */}
-        {/* <span className="author_designation">
-        Content Team
-      </span> */}
-      </div>
-
-    </div>
-  );
-
 };
 
 export default SingleBlogHeroAuthor;
