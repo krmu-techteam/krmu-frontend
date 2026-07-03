a3e71076c891699e306603f98cf0f5f3fe7b3f51
krmu-techteam <tech@krmangalam.com>
2026-07-03 13:08:07 +0530

    fix scroll button issue

--- app/(new-pages)/admission/international/components/BeginYour.tsx
+++ app/(new-pages)/admission/international/components/BeginYour.tsx
@@ -97,14 +97,13 @@ const BeginYour = ({ data, formId }: Props) => {
           Industry-Aligned Courses | Top Recruiters | Global Exposure
         </p>
         <div className="flex flex-col sm:flex-row justify-center gap-4 items-center mb-10">
-          <Link
-            href="#apply-section"
-            // highlightClass="flash-border"
-            className="text-white bg-[linear-gradient(135deg,#e31e24_0%,#c41920_100%)] shadow-[0_4px_14px_0_rgba(227,30,36,0.35)] text-sm md:text-lg min-h-14 inline-flex items-center justify-center gap-2 font-semibold cursor-pointer rounded-xl   transition-all duration-150 ease-out p-3 sm:py-4 sm:px-6 hover:-translate-y-0.5 hover:shadow-[0_6px_20px_0_rgba(227,30,36,0.45)] hover:bg-[linear-gradient(135deg,#ff4d52_0%,#e31e24_100%)]"
+          <ScrollButton
+            targetId="apply-section"
+            highlightClass="flash-border"
+            btnClass="text-white bg-[linear-gradient(135deg,#e31e24_0%,#c41920_100%)] shadow-[0_4px_14px_0_rgba(227,30,36,0.35)] text-sm md:text-lg min-h-14 inline-flex items-center justify-center gap-2 font-semibold cursor-pointer rounded-xl   transition-all duration-150 ease-out p-3 sm:py-4 sm:px-6 hover:-translate-y-0.5 hover:shadow-[0_6px_20px_0_rgba(227,30,36,0.45)] hover:bg-[linear-gradient(135deg,#ff4d52_0%,#e31e24_100%)]"
           >
-            Enrol Now. Limited Seats Available!
-            <ArrowUpRight color="#fff" />
-          </Link>
+            Enrol Now. Limited Seats Available! <ArrowUpRight color="#fff" />
+          </ScrollButton>
 
           <Link
             href="tel:+919311411717"
