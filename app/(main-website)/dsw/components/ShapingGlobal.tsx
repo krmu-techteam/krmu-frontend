a1609b6def0b18966275035239a54a4d5d83062e
krmu-techteam <tech@krmangalam.com>
2026-07-09 17:45:46 +0530

    add temp class

--- app/(main-website)/dsw/components/ShapingGlobal.tsx
+++ app/(main-website)/dsw/components/ShapingGlobal.tsx
@@ -1,7 +1,7 @@
 const ShapingGlobal = () => {
   return (
     <section>
-      <div className="max-w-[675px] mx-auto w-full"></div>
+      <div className="max-w-[675px] mx-auto w-full temp-class"></div>
     </section>
   );
 };
