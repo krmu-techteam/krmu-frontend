1112e5c6dc84b5861a13b1ff909a5507771ad341
vivek verma <vivek459verma@gmail.com>
2026-07-15 17:47:40 +0530

    feat: add new design system sections for career goals, outcomes, prospects, fees, and application details

--- components/school-programmes/programs/new-design-sections/components/feeFinance.tsx
+++ components/school-programmes/programs/new-design-sections/components/feeFinance.tsx
@@ -44,9 +44,9 @@ const FeeFinance = ({ slug }: Props) => {
         </div>
 
         {/* Layout Grid */}
-        <div className="flex flex-col md:flex-row   items-stretch gap-8">
+        <div className="flex flex-col md:flex-row    gap-8">
           {/* Left Card - Program Fee */}
-          <div className="border border-[#8C969E] rounded-[10px] bg-[#F7F0E0] p-6 flex flex-col justify-between transition-shadow duration-300 hover:shadow-[0_4px_25px_rgba(188,176,147,0.15)] w-full sm:max-w-[48%] ">
+          <div className="border border-[#8C969E] rounded-[10px] bg-[#F7F0E0] p-6 flex flex-col justify-between transition-shadow duration-300 hover:shadow-[0_4px_25px_rgba(188,176,147,0.15)] w-full md:max-w-[48%] ">
             <div>
               <h3
                 className={`${fraunces.className} text-2xl md:text-[28px] text-[#002C51]  mb-4`}
@@ -70,7 +70,7 @@ const FeeFinance = ({ slug }: Props) => {
 
               {/* Subtext */}
               <p
-                className={`${inter.className} text-black text-[13px] sm:text-[18px] leading-relaxed mb-6 max-w-[420px]`}
+                className={`${inter.className} text-black text-[13px] sm:text-[18px] leading-relaxed mb-6 w-full md:max-w-[420px]`}
               >
                 {pf.subtext}
               </p>
@@ -104,7 +104,7 @@ const FeeFinance = ({ slug }: Props) => {
           </div>
 
           {/* Right Card - Education-loan partners */}
-          <div className="rounded-[10px] bg-[radial-gradient(50%_50%_at_50%_50%,#024178_0%,#012D52_80%,#012D52_100%)] p-6 px-8 text-white flex flex-col justify-between transition-shadow duration-300 hover:shadow-[0_4px_25px_rgba(1,45,82,0.25)] w-full sm:max-w-[48%] ">
+          <div className="rounded-[10px] bg-[radial-gradient(50%_50%_at_50%_50%,#024178_0%,#012D52_80%,#012D52_100%)] p-6 px-8 text-white flex flex-col justify-between transition-shadow duration-300 hover:shadow-[0_4px_25px_rgba(1,45,82,0.25)] w-full md:max-w-[48%] ">
             <div>
               <h3
                 className={`${fraunces.className} text-2xl md:text-[28px] text-[#E7C268]  mb-4`}
@@ -114,7 +114,7 @@ const FeeFinance = ({ slug }: Props) => {
 
               {/* Description */}
               <p
-                className={`${inter.className} text-[#FDF7EB] text-sm sm:text-[18px] mb-8 max-w-[440px]`}
+                className={`${inter.className} text-[#FDF7EB] text-sm sm:text-[18px] mb-8 w-full md:max-w-[440px]`}
               >
                 {lp.description}
               </p>
