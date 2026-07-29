a1570c4d01e2ee95c181e0529a38daab500e86d0
krmu-techteam <tech@krmangalam.com>
2026-07-29 15:27:01 +0530

    update changes

--- app/(new-pages)/b-tech-cse-robotics-ai/comps/BeyondTheClassroom.tsx
+++ app/(new-pages)/b-tech-cse-robotics-ai/comps/BeyondTheClassroom.tsx
@@ -4,10 +4,10 @@ const BeyondTheClassroom = () => {
   return (
     <section className="py-10 px-5">
       <div className="max-w-7xl mx-auto text-center">
-        <h4 className="text-5xl font-bold text-center mb-5">
+        <h4 className="text-2xl sm:text-3xl lg:text-5xl font-bold text-center mb-5">
           Beyond the Classroom
         </h4>
-        <p>
+        <p className="text-sm sm:text-base">
           Our experiential learning programmes bridge the gap between academics
           and industry by providing hands-on exposure through hackathons, live
           projects, research initiatives, and corporate internships. Students
