e705e6841c74da6f08ca2056dccf0e005348713c
krmu-techteam <tech@krmangalam.com>
2026-07-30 13:16:32 +0530

    add data

--- app/(main-website)/(blogs)/blog/(listings)/layout.tsx
+++ app/(main-website)/(blogs)/blog/(listings)/layout.tsx
@@ -14,9 +14,9 @@ const layout = ({ children }: Props) => {
       <section className="py-[60px] px-4">
         <div className="max-w-[1664px] mx-auto w-full flex flex-col lg:flex-row gap-8">
           {/* LEFT SIDEBAR */}
-          {/* <aside className="w-full lg:w-1/5 order-2 lg:order-1">
+          <aside className="w-full lg:w-1/5 order-2 lg:order-1">
             <CommonBlogSidebar />
-          </aside> */}
+          </aside>
 
           {/* MAIN CONTENT */}
           <main className="w-full lg:w-3/5 order-1 lg:order-2 min-h-screen">
@@ -24,9 +24,9 @@ const layout = ({ children }: Props) => {
           </main>
 
           {/* RIGHT SIDEBAR */}
-          {/* <aside className="w-full lg:w-1/5 order-3">
+          <aside className="w-full lg:w-1/5 order-3">
             <CommonBlogRightSidebar />
-          </aside> */}
+          </aside>
         </div>
       </section>
     </>
