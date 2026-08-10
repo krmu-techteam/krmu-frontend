9e144c439091978140372882c8da7a4bdd75d4b9
krmu-techteam <tech@krmangalam.com>
2026-08-10 12:43:44 +0530

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -309,7 +309,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.7,
     },
     {
-      url: `${baseUrl}/placement/overview`,
+      url: `${baseUrl}/placement`,
       changeFrequency: "monthly",
       priority: 0.7,
     },
