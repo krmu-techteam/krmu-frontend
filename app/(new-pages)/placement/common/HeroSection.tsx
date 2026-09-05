fdba9d3ecb8daff52ee87c4d9b78bb0cddb7125e
krmu-techteam <tech@krmangalam.com>
2026-09-05 11:05:24 +0530

    fix issue

--- app/(new-pages)/placement/common/HeroSection.tsx
+++ app/(new-pages)/placement/common/HeroSection.tsx
@@ -9,7 +9,7 @@ const newsreader = Newsreader({
 const HeroSection = () => {
   return (
     <section>
-      <div className="container mx-auto max-w-[1287px] border-b border-[#ccc] px-4 py-10 sm:px-6 md:py-14 lg:px-8 lg:py-16 xl:py-20">
+      <div className="container mx-auto max-w-[1287px] border-b border-[#ccc] py-10  md:py-14 lg:py-16 xl:py-20">
         <div className="flex flex-col items-center justify-between gap-10 md:flex-row md:gap-8 lg:gap-12">
           {/* Content */}
           <div className="w-full md:w-1/2">
--- app/(new-pages)/placement/common/Registration.tsx
+++ app/(new-pages)/placement/common/Registration.tsx
@@ -1,3 +1,4 @@
+import { Minus } from "lucide-react";
 import { Newsreader } from "next/font/google";
 
 const newsreader = Newsreader({
@@ -8,12 +9,31 @@ const newsreader = Newsreader({
 const Registration = () => {
   return (
     <section>
-      <div className="container mx-auto max-w-[1287px] border-b border-[#ccc] px-4 py-10 sm:px-6 md:py-14 lg:px-8 lg:py-16 xl:py-20">
+      <div className="container mx-auto max-w-[1287px] border-b border-[#ccc] py-10  md:py-14 lg:py-16 xl:py-20">
         <h2
           className={`${newsreader.className} mb-5 text-4xl font-medium leading-[1.15] sm:text-5xl md:text-[48px] lg:text-6xl lg:leading-[70px]`}
         >
           Registration details
         </h2>
+        <div>
+          <h4 className="text-base text-[#7A1F2B] font-medium tracking-widest leading-snug">
+            Eligibility
+          </h4>
+          <ul className="[&>*]:border-t [&>*]:border-[]">
+            <li className="flex items-start gap-2">
+              <Minus />
+              <span>Final year students with zero active backlogs.</span>
+            </li>
+            <li className="flex items-start gap-2">
+              <Minus />
+              <span>Minimum CGPA of 6.5 across all semesters.</span>
+            </li>
+            <li className="flex items-start gap-2">
+              <Minus />
+              <span>Completed mandatory pre-placement training.</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
