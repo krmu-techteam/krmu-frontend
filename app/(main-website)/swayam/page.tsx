55e8353edf1e544d3d4ff2e6f6b60beb1feeba1b
princecodecfart <princesharmabbu0@gmail.com>
2026-09-07 11:54:54 +0530

    feat: add hidden Swayam static page route with conditional rendering

--- app/(main-website)/swayam/page.tsx
+++ app/(main-website)/swayam/page.tsx
@@ -1,4 +1,8 @@
+import { notFound } from "next/navigation";
 import SwayamStaticPage from "@/src/presentation/static-pages/swayam";
+
+// Set to true to make the page live
+const isLive = false;
 
 export const metadata = {
   title: "SWAYAM - K.R. Mangalam University",
@@ -7,6 +11,10 @@ export const metadata = {
 };
 
 const SwayamPage = () => {
+  if (!isLive) {
+    notFound();
+  }
+
   return <SwayamStaticPage />;
 };
 
