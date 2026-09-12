b2fe301e8e9a580ea77163fb93a9d1ce30a79496
krmu-techteam <tech@krmangalam.com>
2026-09-12 10:06:59 +0530

    update changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MKXDVSJ9');
+            })(window,document,'script','dataLayer','GTM-P8ZKK9X');
           `}
         </Script>
         {/* <Script
@@ -59,7 +59,7 @@ export default function RootLayout({
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MKXDVSJ9"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P8ZKK9X"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
