cca32bb1f0da080a5bb858dd73dfccb73a8a2f23
rbabucoder <baburohit0131@gmail.com>
2026-09-21 22:10:07 +0530

    test working

--- app/(main-website)/(blogs)/blog/(listings)/page.tsx
+++ app/(main-website)/(blogs)/blog/(listings)/page.tsx
@@ -1,32 +1,38 @@
-import { getBlogPageInfo } from "@/lib/api/blogs/main-blog";
-// import CommonBlogHeroSection from "./comp/CommonBlogHeroSection";
-import CommonBlogLayout from "./comp/CommonBlogLayout";
-import { strapiSeoToMetadata } from "@/lib/constants/strapiMeta";
-// import CommonBlogHeroSection from "./comp/CommonBlogHeroSection";
-
-type Props = {
-  searchParams: Promise<{ page?: string }>;
-};
-// ---------------------------
-// ✅ Metadata
-// ---------------------------
-export async function generateMetadata() {
-  const pageInfo = await getBlogPageInfo();
-  const seo = pageInfo?.blog_seo;
-
-
-  if (!seo) return {};
-
-  return strapiSeoToMetadata(seo);
-}
-
-const Page = async ({ searchParams }: Props) => {
-  return (
-    <>
-      {/* <CommonBlogHeroSection /> */}
-      <CommonBlogLayout searchParams={searchParams} mainBlogClass={"grid grid-cols-1 xl:grid-cols-2 gap-6"} />
-    </>
-  );
+const Page = async () => {
+  return <div>TEST BLOG PAGE</div>;
 };
 
 export default Page;
+
+// import { getBlogPageInfo } from "@/lib/api/blogs/main-blog";
+// // import CommonBlogHeroSection from "./comp/CommonBlogHeroSection";
+// import CommonBlogLayout from "./comp/CommonBlogLayout";
+// import { strapiSeoToMetadata } from "@/lib/constants/strapiMeta";
+// // import CommonBlogHeroSection from "./comp/CommonBlogHeroSection";
+
+// type Props = {
+//   searchParams: Promise<{ page?: string }>;
+// };
+// // ---------------------------
+// // ✅ Metadata
+// // ---------------------------
+// export async function generateMetadata() {
+//   const pageInfo = await getBlogPageInfo();
+//   const seo = pageInfo?.blog_seo;
+
+
+//   if (!seo) return {};
+
+//   return strapiSeoToMetadata(seo);
+// }
+
+// const Page = async ({ searchParams }: Props) => {
+//   return (
+//     <>
+//       {/* <CommonBlogHeroSection /> */}
+//       <CommonBlogLayout searchParams={searchParams} mainBlogClass={"grid grid-cols-1 xl:grid-cols-2 gap-6"} />
+//     </>
+//   );
+// };
+
+// export default Page;
