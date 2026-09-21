8b23f0dbd52496c917020fa9db554382d9ab88a1
rbabucoder <baburohit0131@gmail.com>
2026-09-21 22:27:44 +0530

    try to fix

--- app/(main-website)/(blogs)/blog/(listings)/page.tsx
+++ app/(main-website)/(blogs)/blog/(listings)/page.tsx
@@ -1,44 +1,38 @@
-const Page = async () => {
-  return <div>TEST BLOG PAGE</div>;
+import { getBlogPageInfo } from "@/lib/api/blogs/main-blog";
+import CommonBlogLayout from "./comp/CommonBlogLayout";
+import { strapiSeoToMetadata } from "@/lib/constants/strapiMeta";
+
+type Props = {
+  searchParams: Promise<{
+    page?: string;
+  }>;
+};
+
+// ------------------------------------
+// Metadata
+// ------------------------------------
+export async function generateMetadata() {
+  const pageInfo = await getBlogPageInfo();
+
+  const seo = pageInfo?.blog_seo;
+
+  if (!seo) {
+    return {};
+  }
+
+  return strapiSeoToMetadata(seo);
+}
+
+// ------------------------------------
+// Page
+// ------------------------------------
+const Page = async ({ searchParams }: Props) => {
+  return (
+    <CommonBlogLayout
+      searchParams={searchParams}
+      mainBlogClass="grid grid-cols-1 xl:grid-cols-2 gap-6"
+    />
+  );
 };
 
 export default Page;
-
-// import { getBlogPageInfo } from "@/lib/api/blogs/main-blog";
-// import CommonBlogLayout from "./comp/CommonBlogLayout";
-// import { strapiSeoToMetadata } from "@/lib/constants/strapiMeta";
-
-// type Props = {
-//   searchParams: Promise<{
-//     page?: string;
-//   }>;
-// };
-
-// // ------------------------------------
-// // Metadata
-// // ------------------------------------
-// export async function generateMetadata() {
-//   const pageInfo = await getBlogPageInfo();
-
-//   const seo = pageInfo?.blog_seo;
-
-//   if (!seo) {
-//     return {};
-//   }
-
-//   return strapiSeoToMetadata(seo);
-// }
-
-// // ------------------------------------
-// // Page
-// // ------------------------------------
-// const Page = async ({ searchParams }: Props) => {
-//   return (
-//     <CommonBlogLayout
-//       searchParams={searchParams}
-//       mainBlogClass="grid grid-cols-1 xl:grid-cols-2 gap-6"
-//     />
-//   );
-// };
-
-// export default Page;
