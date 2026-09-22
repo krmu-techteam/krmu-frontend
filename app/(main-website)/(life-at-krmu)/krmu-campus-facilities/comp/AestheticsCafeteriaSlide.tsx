50674e0df98efb0cae6d58fe126f3fcf73b0d1c7
krmu-techteam <tech@krmangalam.com>
2026-09-22 13:13:06 +0530

    update

--- app/(main-website)/(life-at-krmu)/krmu-campus-facilities/comp/AestheticsCafeteriaSlide.tsx
+++ app/(main-website)/(life-at-krmu)/krmu-campus-facilities/comp/AestheticsCafeteriaSlide.tsx
@@ -36,7 +36,7 @@ const AestheticsCafeteriaSlide = ({ data }: Props) => {
           );
         })}
       </CarouselContent>
-      <CarouselPrevious className="left-0 rounded-none bg-black text-white temp-class" />
+      <CarouselPrevious className="left-0 rounded-none bg-black text-white" />
       <CarouselNext className="right-0 rounded-none bg-black text-white" />
     </Carousel>
   );
